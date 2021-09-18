a8908e57b56d9a0d5099dc226238ab9f08883b0e
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-18 21:09:06 +0500

    Update
    Update

--- basic_funcs/dialation_image.py
+++ basic_funcs/dialation_image.py
@@ -6,7 +6,10 @@ kernal = np.ones((2, 2), np.uint8)
 
 """
 What this function does it makes the edges of the functions thick 
-so that the canny function can process them in a better way.
+so that the canny function can process them in a better way, and also
+numpy is used here in the kernal variable to do that main part which is 
+deciding the amount of thinkness needed for canny to process the lines of 
+the edges in a better way
 """    
 imgCanny = cv2.Canny(img, 150, 200)
 imgDialation = cv2.dilate(imgCanny, kernal)
